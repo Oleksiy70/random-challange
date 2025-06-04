98d4f1f7d3e9b78423c4b161b75be8567d987a86
Oleksiy70 <o.v.lohoyda@gmail.com>
2025-06-04 23:17:30 +0300

    zxc11

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,4 @@
+// server/config/db.js
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
@@ -7,8 +8,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,          // Додано порт
-    dialect: 'postgres',                // Заміна mysql на postgres
+    dialect: 'mysql',
     logging: false,
   }
 );
